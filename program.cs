60506538e02bb8a6ea9cae4ec9da4ec2c7248912
mikhail <mikhail.semchinov@gmail.com>
2023-04-08 19:32:22 +0300

    first program

---
+++ program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace СSharpGeekBrains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+
+            Console.WriteLine("Введите число элементов массива");
+            N = Int32.Parse(Console.ReadLine());
+
+            string[] words = new string[N];
+
+            Console.WriteLine("Введите элементы массива");
+
+            from (int i = 0, i < N, i++)
+            {
+                words[i] = ReadLine();
+            }
+
+
+
+        }
+    }
+}
